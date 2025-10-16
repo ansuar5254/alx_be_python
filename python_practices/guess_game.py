d3864d02792c2083cc4a8243cb028a8534367fa0
ansuar5254 <ansuar5254@gmail.com>
2025-10-16 19:22:21 +0300

    This medicalpython -u c:\Users\muham\OneDrive\Documents\python\python_practices\guess_game.py

--- python_practices/guess_game.py
+++ python_practices/guess_game.py
@@ -1,10 +1,10 @@
 import random
-secret_num = random.randint(1,10)
+secret_num = random.randint(1,100)
 cho = 'y'
 print("I am thinking of a number between 1 and 10. can u gusee it?")
 while(cho == 'y'):
- for i in range(3):
-    guess = int(input(f"You have {3 - i} trial(s): "))
+ for i in range(10):
+    guess = int(input(f"You have {10 - i} trial(s): "))
     if secret_num == guess:
         print("Congratulations, you guessed it!")
         break
@@ -12,5 +12,9 @@ while(cho == 'y'):
         print("Nope, your guess is a bit high. Give it another shot!")
     else:
         print("Nope, your guess is a bit low. Give it another shot!")
-    
- cho = input("do you want to contnue?(y/n)")
+ else:
+    print(f"sorry, the numbere was {secret_num}")
+ cho = input("do you want to contnue?(y/n)").lower()
+ if cho == 'y':
+    secret_num = random.randint(1,10)
+
